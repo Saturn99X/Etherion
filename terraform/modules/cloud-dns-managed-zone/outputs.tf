5d3548293b6af077c0b005d9e2367ed477aa6e44
SaturnX <triomphengala@gmail.com>
2026-03-26 23:53:48 +0000

    remove frontend_templates, terraform, spend-guard, github workflows

--- terraform/modules/cloud-dns-managed-zone/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_name" {
-  description = "Managed zone name"
-  value       = google_dns_managed_zone.primary.name
-}
-
-output "name_servers" {
-  description = "List of Cloud DNS nameservers for the zone"
-  value       = google_dns_managed_zone.primary.name_servers
-}
--- terraform/modules/cloud-run-frontend/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "service_name" {
-  value = google_cloud_run_v2_service.frontend.name
-}
-
-output "service_account_email" {
-  value = google_service_account.fe_service.email
-}
